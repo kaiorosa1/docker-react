2ca5954807f9fc57c00f10ecf875ec8de37fbf46
Kaio Rosa <kaiosrosa@gmail.com>
2024-01-22 18:03:49 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React/ We are using Docker to run it
         </a>
       </header>
     </div>
